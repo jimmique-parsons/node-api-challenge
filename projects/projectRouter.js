314887236f8b72008f18cd73008d1277d8b7332d
Jimmique <jimmique88@gmail.com>
2020-01-24 19:51:31 -0800

    Added validateProjectId error handler and GET handlers, and added validateProjectBody error handler and POST request to /

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -12,4 +12,58 @@ router.get('/', async (req, res, next) => {
         : next({ status: 500, message: 'Error retrieving projects' });
 });
 
+router.get('/:id', validateProjectId, async (req, res, next) => {
+    res.status(200).json(req.project);
+});
+
+router.get('/:id/actions', validateProjectId, async (req, res, next) => {
+    try {
+        const actions = await Projects.getProjectActions(req.project.id);
+        res.status(200).json(actions);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/', validateProjectBody, async (req, res, next) => {
+    try {
+        const result = await Projects.insert(req.project);
+        res.status(201).json(result);
+    } catch (error) {
+        next(error);
+    }
+});
+
+async function validateProjectId(req, res, next) {
+    const { id } = req.params;
+    try {
+        const project = await Projects.get(id);
+
+        if (project) {
+            req.project = project;
+            next();
+        } else {
+            next({ status: 404, message: 'Project not found' });
+        }
+    } catch (error) {
+        next(error);
+    }
+
+};
+
+async function validateProjectBody(req, res, next) {
+    const { body } = req;
+
+    if (Object.keys(body).length === 0) {
+        next({ status: 400, message: "Missing project info" });
+    } else if (!body.name) {
+        next({ status: 400, message: "Project name required" });
+    } else if (!body.description) {
+        next({ status: 400, message: "Project description required" });
+    } else {
+        req.project = body;
+        next();
+    }
+}
+
 module.exports = router;
